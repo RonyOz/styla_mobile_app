006fac11e43bf2807cec1b2d205d64677903d069
Rony Ordoñez <ronyfarido@gmail.com>
2025-11-08 10:36:55 -0500

    feat: update core exports to include app_radius and additional widgets

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -2,6 +2,7 @@
 export 'ui/design/app_colors.dart';
 export 'ui/design/app_typography.dart';
 export 'ui/design/app_spacing.dart';
+export 'ui/design/app_radius.dart';
 
 // Theme
 export 'ui/theme/app_theme.dart';
@@ -10,3 +11,5 @@ export 'ui/theme/app_theme.dart';
 export 'ui/widgets/app_button.dart';
 export 'ui/widgets/app_text_field.dart';
 export 'ui/widgets/app_image.dart';
+export 'ui/widgets/app_chip_button.dart';
+export 'ui/widgets/app_toggle_pill.dart';
